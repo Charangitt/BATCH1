a794c80b87ed6149a4b55e8136706e10a9070f6f
Charangitt <160092118+Charangitt@users.noreply.github.com>
2025-11-05 11:49:06 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,14 +1,5 @@
 public class factorial {
-    public static int factorial(int n) {
-        if (n == 0 || n == 1)
-            return 1;
-        else
-            return n * factorial(n - 1);
-    }
-
     public static void main(String[] args) {
-        int number = 5; // Change this to any number you want
-        int result = factorial(number);
-        System.out.println("Factorial of " + number + " is: " + result);
+        System.out.println("hello world");
     }
 }
